22d435ed38d6513fc2c7216fd47b5d6782a308dc
alon muroch <alonmuroch@gmail.com>
2015-02-26 11:58:01 +0200

    init

---
+++ authenticator/authenticator/authenticator.h
@@ -0,0 +1,13 @@
+//
+//  authenticator.h
+//  authenticator
+//
+//  Created by alon muroch on 2/26/15.
+//  Copyright (c) 2015 Bitcoin Authenticator. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface authenticator : NSObject
+
+@end
---
+++ authenticator/authenticator/authenticator.m
@@ -0,0 +1,13 @@
+//
+//  authenticator.m
+//  authenticator
+//
+//  Created by alon muroch on 2/26/15.
+//  Copyright (c) 2015 Bitcoin Authenticator. All rights reserved.
+//
+
+#import "authenticator.h"
+
+@implementation authenticator
+
+@end
